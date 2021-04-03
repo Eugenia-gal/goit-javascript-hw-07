564518e4c8924abe9d5b8f4d082908d1a73c6455
Eugenia-gal <galevbor12@gmail.com>
2021-04-03 12:35:39 +0300

    adds markup and script for task-01

---
+++ js/task-01.js
@@ -0,0 +1,26 @@
+/* Задание Напиши скрипт, который выполнит следующие операции.
+Посчитает и выведет в консоль количество категорий в ul#categories, 
+то есть элементов li.item. Получится 'В списке 3 категории.'.
+
+Для каждого элемента li.item в списке ul#categories, 
+найдет и выведет в консоль текст заголовка элемента (тега h2) 
+и количество элементов в категории (всех вложенных в него элементов li).
+
+Например для первой категории получится:
+
+Категория: Животные
+Количество элементов: 4 */
+
+const categoriesListEl = document.querySelectorAll('.item');
+console.log(`В списке ${categoriesListEl.length} категории.`)
+
+function getCategoryDescription (category) {
+    const categoryNameEl = category.querySelector('h2');
+    const categoryItemsEl = category.querySelectorAll('li');
+
+    console.log(
+`Категория: ${categoryNameEl.textContent}
+Количество элементов: ${categoryItemsEl.length}`);
+}
+
+categoriesListEl.forEach(getCategoryDescription);
